3bf8f9258b6538830c0acf5be4ba3705556f11bb
allu <aleksijaakkojuhani@gmail.com>
2020-08-11 10:57:36 +0300

    action menu

--- src/screens/ActionButton.js
+++ src/screens/ActionButton.js
@@ -38,21 +38,50 @@ const ActionButton = () => {
     ]
   }
 
+  const labelPositionInterpolate = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-30, -90]
+  });
+
+  const labelOpacityInterpolate = animation.interpolate({
+    inputRange: [0, .5, 1],
+    outputRange: [0, 0, 1]
+  })
+
+  const labelStyle = {
+    transform: [
+      { translateX: labelPositionInterpolate}
+    ],
+    opacity: labelOpacityInterpolate
+  }
+
+  const backgroundStyle = {
+    transform: [{
+      scale: animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 23]
+      })
+    }]
+  }
+
   return (
     <View style={styles.container}>
-
+      <Animated.View style={[styles.background, backgroundStyle]} />
       <TouchableWithoutFeedback>
         <Animated.View style={[styles.button, styles.otherButton, orderStyle]}>
+          <Animated.Text style={[styles.label, labelStyle]}>Order</Animated.Text>
           <Icon name="food-fork-drink" size={20} color="#555" />
         </Animated.View>
       </TouchableWithoutFeedback>
       <TouchableWithoutFeedback>
         <Animated.View style={[styles.button, styles.otherButton, reloadStyle]}>
+          <Animated.Text style={[styles.label, labelStyle]}>Reload</Animated.Text>
           <Icon name="reload" size={20} color="#555" />
         </Animated.View>
       </TouchableWithoutFeedback>
       <TouchableWithoutFeedback onPress={toggleOpen}>
         <View style={[styles.button, styles.pay]}>
+          <Animated.Text style={[styles.label, labelStyle]}>Pay</Animated.Text>
           <Text style={styles.payText}>5,00€</Text>
         </View>
       </TouchableWithoutFeedback>
@@ -63,7 +92,6 @@ const ActionButton = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //justifyContent: "center"
   },
   button: {
     width: 60,
@@ -71,9 +99,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     shadowColor: "#333",
-    shadowOpacity: .1,
+    shadowOpacity: .4,
     shadowOffset: { x: 2, y: 0 },
-    shadowRadius: 2,
+    shadowRadius: 3.5,
     borderRadius: 30, 
     position: 'absolute',
     bottom: 20,
@@ -88,6 +116,22 @@ const styles = StyleSheet.create({
   payText: {
     color: "#FFF",
   },
+  label: {
+    color: "#FFF",
+    position: "absolute",
+    fontSize: 18,
+    backgroundColor: "transparent",
+    fontWeight: "bold",
+  },
+  background: {
+    backgroundColor: "rgba(0,0,0,.3)",
+    position: "absolute",
+    width: 60,
+    height: 60,
+    bottom: 20,
+    right: 20,
+    borderRadius: 30,
+  }
 })
 
 export default ActionButton;
